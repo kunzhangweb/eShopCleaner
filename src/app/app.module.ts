4b5b97b2a2ec8e9c435f71ef13a78a37ede63ec5
Kun Zhang <kzgoldenwing@gmail.com>
2023-05-27 01:05:35 -0400

    remove angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { LoginComponent } from "../app/components/login/login.component";
 import { SignupComponent } from "../app/components/signup/signup.component";
 
 import { AngularFireModule } from "@angular/fire";
-import { AngularFireAuthModule } from "@angular/fire/auth";
+// import { AngularFireAuthModule } from "@angular/fire/auth";
 // import { CleanerComponent } from "./components/cleaner/cleaner.component";
 import { CleanerAddComponent } from "./components/cleaner-add/cleaner-add.component";
 import { CleanerDetailComponent } from "./components/cleaner-detail/cleaner-detail.component";
@@ -33,14 +33,14 @@ import { CleanerListComponent } from "./components/cleaner-list/cleaner-list.com
 import { AdminSignupComponent } from "./components/admin-signup/admin-signup.component";
 import { HeaderComponent } from "./components/header/header.component";
 
-var config = {
-  apiKey: "AIzaSyADiXXXXXXXXXXXXXXHT-IY",
-  authDomain: "ionic-4-fireXXXXXXXXXXeapp.com",
-  databaseURL: "https://ionic-4-fireXXXXXXXXXXXXeio.com",
-  projectId: "ionic-4-fiXXXXXXXXXXXn",
-  storageBucket: "ionic-4-firXXXXXXXXXXXpot.com",
-  messagingSenderId: "9253XXXXXXX324",
-};
+// var config = {
+//   apiKey: "AIzaSyADiXXXXXXXXXXXXXXHT-IY",
+//   authDomain: "ionic-4-fireXXXXXXXXXXeapp.com",
+//   databaseURL: "https://ionic-4-fireXXXXXXXXXXXXeio.com",
+//   projectId: "ionic-4-fiXXXXXXXXXXXn",
+//   storageBucket: "ionic-4-firXXXXXXXXXXXpot.com",
+//   messagingSenderId: "9253XXXXXXX324",
+// };
 
 @NgModule({
   declarations: [
@@ -72,7 +72,7 @@ var config = {
     MatRadioModule,
     FlexLayoutModule,
     MatSidenavModule,
-    AngularFireModule.initializeApp(config),
+    // AngularFireModule.initializeApp(config),
     AngularFireAuthModule,
   ],
   providers: [],
